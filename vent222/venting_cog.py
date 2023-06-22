edae776da8ebde71457bf9fcabe0712087b8800e
valor <61257192+taintedvalor@users.noreply.github.com>
2023-06-22 16:42:30 -0400

    Add files via upload

---
+++ vent222/venting_cog.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+class VentCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def vent(self, ctx, guild_id: int, *, message):
+        guild = self.bot.get_guild(guild_id)
+        if guild is None:
+            await ctx.send("Target guild not found.")
+            return
+
+        channel = discord.utils.get(guild.channels, name="venting")
+        if channel is None:
+            await ctx.send("Target channel not found.")
+            return
+
+        try:
+            await channel.send(f"Vent from {ctx.author.name}: {message}")
+            await ctx.send("Message vented successfully!")
+        except discord.Forbidden:
+            await ctx.send("Bot doesn't have permission to send messages in the target channel.")
+        except discord.HTTPException:
+            await ctx.send("An error occurred while venting the message. Please try again later.")
+
+def setup(bot):
+    bot.add_cog(VentCog(bot))
