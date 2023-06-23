3e10a2677f0530d563e24ce3445b4c1b926a482a
valor <61257192+taintedvalor@users.noreply.github.com>
2023-06-23 09:07:05 -0400

    Add files via upload

---
+++ venter/venting_cog.py
@@ -0,0 +1,32 @@
+import discord
+from redbot.core import commands, checks, bot
+from random import choice as randchoice
+from pprint import pprint as pp
+from typing import List, Dict
+from fuzzywuzzy import process
+
+BaseCog = getattr(commands, "Cog", object)
+
+
+class VentCog(commands.Cog):
+    def __init__(self, bot_instance: bot):
+        self.bot = bot_instance
+        """Cog for venting Anonymously in dms"""
+
+    @commands.command(pass_context=True, rest_is_raw=True)
+    async def vent(self, ctx, guild_id: int, *, message: str):
+        """Vent Anonymously from Dms to a specific guild in a 🌬-venting-room"""
+        target_guild = self.bot.get_guild(guild_id)
+        if not target_guild:
+            return await ctx.send("Target guild not found.")
+
+        target_channel = discord.utils.get(target_guild.text_channels, name="🌬-venting-room")
+        if not target_channel:
+            return await ctx.send("No ventingroom channel in the given guild.")
+
+        await target_channel.send(f"Anonymous Person:{message}")
+        await ctx.send("Your message has been Sent.")
+
+
+def setup(bot):
+    bot.add_cog(VentCog(bot))
