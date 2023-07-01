6b0b3300b7492243561f555affd09bba751eccc7
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-01 18:51:46 -0400

    Add files via upload

---
+++ Tod/truthOrDare.py
@@ -0,0 +1,97 @@
+import discord
+from redbot.core import commands
+import random
+import asyncio
+
+class TruthOrDare(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def truth_or_dare(self, ctx):
+        messages = {
+            "truth": [
+                "Have you ever lied to your best friend?",
+                "What is your biggest fear?",
+                "What's the most embarrassing thing you've done?",
+            ],
+            "dare": [
+                "Sing a song in a funny voice.",
+                "Do 10 push-ups right now!",
+                "Eat a spoonful of mustard.",
+            ],
+            "nsfw_truth": [
+                "What is your wildest fantasy?",
+                "Have you ever had a one-night stand?",
+                "What is your favorite position?",
+            ],
+            "nsfw_dare": [
+                "Send a risky message to someone you like.",
+                "Do a seductive dance.",
+                "Give a lap dance to someone in the room (if appropriate).",
+            ],
+        }
+        message = await ctx.send(f"Click a button to get a random question.")
+        truth_emoji = "🎲"
+        dare_emoji = "🎯"
+        nsfw_emoji = "🔞"
+        await message.add_reaction(truth_emoji)  # Truth
+        await message.add_reaction(dare_emoji)  # Dare
+        await message.add_reaction(nsfw_emoji)  # NSFW
+
+        def reaction_check(reaction, user):
+            return (
+                user == ctx.author
+                and reaction.message.id == message.id
+                and str(reaction.emoji) in [truth_emoji, dare_emoji, nsfw_emoji]
+            )
+
+        while True:
+            try:
+                reaction, _ = await self.bot.wait_for(
+                    "reaction_add", check=reaction_check, timeout=30.0
+                )
+            except asyncio.TimeoutError:
+                await ctx.send("You didn't click a button in time.")
+                break
+
+            if str(reaction.emoji) == truth_emoji:
+                random_message = random.choice(messages["truth"])
+                await ctx.send(f"Your random truth question: {random_message}")
+            elif str(reaction.emoji) == dare_emoji:
+                random_message = random.choice(messages["dare"])
+                await ctx.send(f"Your random dare question: {random_message}")
+            elif str(reaction.emoji) == nsfw_emoji:
+                if ctx.channel.is_nsfw():
+                    nsfw_reactions = ["💣", "💥"]  # NSFW reactions to swap to
+                    nsfw_message = await ctx.send(f"Click a button to get a random NSFW question.")
+                    await nsfw_message.add_reaction(nsfw_reactions[0])  # NSFW Truth
+                    await nsfw_message.add_reaction(nsfw_reactions[1])  # NSFW Dare
+
+                    def nsfw_reaction_check(nsfw_reaction, nsfw_user):
+                        return (
+                            nsfw_user == ctx.author
+                            and nsfw_reaction.message.id == nsfw_message.id
+                            and str(nsfw_reaction.emoji) in nsfw_reactions
+                        )
+
+                    while True:
+                        try:
+                            nsfw_reaction, _ = await self.bot.wait_for(
+                                "reaction_add", check=nsfw_reaction_check, timeout=30.0
+                            )
+                        except asyncio.TimeoutError:
+                            await ctx.send("You didn't click a button in time.")
+                            break
+
+                        if str(nsfw_reaction.emoji) == nsfw_reactions[0]:
+                            random_message = random.choice(messages["nsfw_truth"])
+                            await ctx.send(f"Your random NSFW truth question: {random_message}")
+                        elif str(nsfw_reaction.emoji) == nsfw_reactions[1]:
+                            random_message = random.choice(messages["nsfw_dare"])
+                            await ctx.send(f"Your random NSFW dare question: {random_message}")
+                else:
+                    await ctx.send("This command is not available in non-NSFW channels.")
+
+def setup(bot):
+    bot.add_cog(TruthOrDare(bot))
