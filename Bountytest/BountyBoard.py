4c757218c7942fe433ea5a6958b5315bfcce3904
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-05 13:34:23 -0500

    Add files via upload

--- Bountytest/BountyBoard.py
+++ Bountytest/BountyBoard.py
@@ -21,7 +21,6 @@ class BountyBoard(commands.Cog):
         self.config = Config.get_conf(self, identifier=1234567890)
         default_guild_config = {
             "supervisor_role": None,
-            "bounty_channel": None,
             "toggle_reward": True,
             "toggle_supervisor": True
         }
@@ -29,35 +28,13 @@ class BountyBoard(commands.Cog):
         self.bounties = []
         self.stars = {}  # Dictionary to store star counts
 
-    async def send_bounty_board(self, guild, channel):
-        embed = discord.Embed(title="Bounty Board", description="Active bounties")
-
-        for bounty in self.bounties:
-            embed.add_field(
-                name=bounty.name,
-                value=f"Poster: {bounty.poster.mention}\nDescription: {bounty.description}\nReward: {bounty.reward}",
-                inline=False
-            )
-
-        await channel.send(embed=embed)
-
-    async def take_bounty(self, ctx, bounty):
-        if bounty.taken:
-            await ctx.send("This bounty has already been taken.")
-            return
-
-        bounty.taken = True
-        bounty.taken_by = ctx.author
-
-        await ctx.send(f"{ctx.author.mention} has taken the bounty '{bounty.name}'.")
-        await bounty.poster.send(f"{ctx.author.mention} has taken your bounty '{bounty.name}'.")
-
     @commands.group(name="bounty", aliases=["bounties"], invoke_without_command=True)
     async def bounty_group(self, ctx):
         """Manage bounties"""
         await ctx.send_help()
 
     @bounty_group.group(name="toggle", invoke_without_command=True)
+    @commands.guild_only()
     @checks.admin_or_permissions(manage_guild=True)
     async def toggle_group(self, ctx):
         """Toggle bounty options"""
@@ -101,7 +78,8 @@ class BountyBoard(commands.Cog):
         toggle_supervisor = await self.config.guild(ctx.guild).toggle_supervisor()
         if toggle_supervisor:
             supervisor = None  # Assign None as the default value
-            supervisor_msg = await ctx.send("Would you like to assign a supervisor for this bounty? (Type 'yes' or 'no')")
+            supervisor_msg = await ctx.send("```python
+            Would you like to assign a supervisor for this bounty? (Type 'yes' or 'no')")
             supervisor_response = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
 
             if supervisor_response.content.lower() == "yes":
@@ -156,77 +134,74 @@ class BountyBoard(commands.Cog):
 
         await ctx.send("Bounty not found!")
 
+    @bounty_group.command(name="take")
+    async def take_bounty(self, ctx, bounty_name):
+        for bounty in self.bounties:
+            if bounty.name.lower() == bounty_name.lower():
+                if not bounty.completed and not bounty.taken:
+                    bounty.taken = True
+                    bounty.taken_by = ctx.author
+
+                    await ctx.send(f"{ctx.author.mention} has taken the bounty '{bounty.name}'!")
+
+                    # Notify the bounty poster
+                    await bounty.poster.send(f"Your bounty '{bounty.name}' has been taken by {ctx.author.mention}!")
+
+                    return
+
+        await ctx.send("Bounty not found or already completed/taken!")
+
     @bounty_group.command(name="board")
     async def bounty_board(self, ctx):
         await self.send_bounty_board(ctx.guild, ctx.channel)
 
-    @bounty_group.command(name="take")
-    async def take_bounty_command(self, ctx, bounty_name):
-        for bounty in self.bounties:
-            if bounty.name.lower() == bounty_name.lower():
-                if not bounty.taken:
-                    await self.take_bounty(ctx, bounty)
-                else:
-                    await ctx.send("This bounty has already been taken.")
-                return
-
-        await ctx.send("Bounty not found!")
-
     def add_star(self, user):
-        if user.id not in self.stars:
-            self.stars[user.id] = 0
-        self.stars[user.id] += 1
-
-    def remove_star(self, user):
-        if user.id in self.stars:
-            self.stars[user.id] -= 1
-            if self.stars[user.id] <= 0:
-                del self.stars[user.id]
+        if str(user.id) not in self.stars:
+            self.stars[str(user.id)] = 1
+        else:
+            self.stars[str(user.id)] += 1
 
     @bounty_group.command(name="stars")
-    async def user_stars(self, ctx, user: discord.Member = None):
-        user = user or ctx.author
-        star_count = self.stars.get(user.id, 0)
-        await ctx.send(f"{user.mention} has {star_count} stars.")
+    async def stars(self,```python
+    @bounty_group.command(name="stars")
+    async def stars(self, ctx):
+        user_id = str(ctx.author.id)
+        star_count = self.stars.get(user_id, 0)
+        await ctx.send(f"{ctx.author.mention} has {star_count} stars.")
 
-    @bounty_group.command(name="addstar")
-    @checks.is_owner()
-    async def add_star_command(self, ctx, user: discord.Member):
-        self.add_star(user)
-        await ctx.send(f"{user.mention} has been rewarded with a star!")
+    @bounty_group.command(name="top")
+    async def top_stars(self, ctx):
+        sorted_stars = sorted(self.stars.items(), key=lambda x: x[1], reverse=True)
 
-    @bounty_group.command(name="removestar")
-    @checks.is_owner()
-    async def remove_star_command(self, ctx, user: discord.Member):
-        self.remove_star(user)
-        await ctx.send(f"A star has been removed from {user.mention}.")
+        if not sorted_stars:
+            await ctx.send("No stars have been awarded yet.")
+            return
 
-    @commands.group(name="bountyset")
-    @checks.admin_or_permissions(manage_guild=True)
-    async def bountyset_group(self, ctx):
-        """Manage bounty settings"""
-        pass
+        embed = discord.Embed(title="Top Star Counts", description="")
 
-    @bountyset_group.command(name="supervisor")
-    async def set_supervisor_role(self, ctx, role: discord.Role):
-        """Set the supervisor role for bounties"""
-        await self.config.guild(ctx.guild).supervisor_role.set(role.id)
-        await ctx.send(f"The supervisor role for bounties has been set to {role.name}.")
+        for idx, (user_id, star_count) in enumerate(sorted_stars[:10]):
+            user = ctx.guild.get_member(int(user_id))
+            if user:
+                embed.add_field(
+                    name=f"**{idx + 1}. {user.display_name}**",
+                    value=f"Star Count: {star_count}",
+                    inline=False
+                )
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        for guild in self.bot.guilds:
-            bounty_channel_id = await self.config.guild(guild).bounty_channel()
-            if bounty_channel_id:
-                bounty_channel = guild.get_channel(bounty_channel_id)
-                if bounty_channel:
-                    await self.send_bounty_board(guild, bounty_channel)
+        await ctx.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.errors.CheckFailure):
-            await ctx.send("You don't have permission to use this command.")
+    async def send_bounty_board(self, guild, channel):
+        embed = discord.Embed(title="Bounty Board", description="Active bounties")
 
-# Setup function for Red
+        for bounty in self.bounties:
+            embed.add_field(
+                name=bounty.name,
+                value=f"Poster: {bounty.poster.mention}\nDescription: {bounty.description}\nReward: {bounty.reward}",
+                inline=False
+            )
+
+        await channel.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(BountyBoard(bot))
