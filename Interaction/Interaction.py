a05a1de736ca0f5eeba8c9cdfb0567e3b2e5acf9
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-09 19:33:45 -0400

    Update Interaction.py

--- Interaction/Interaction.py
+++ Interaction/Interaction.py
@@ -11,58 +11,61 @@ class InteractionsCog(commands.Cog):
 
     @commands.command()
     async def hug(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime hug')
+        gif_url = await self.get_random_anime_gif('anime hug', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} hugged {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def kiss(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime kiss')
+        gif_url = await self.get_random_anime_gif('anime kiss', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} kissed {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def slap(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime slap')
+        gif_url = await self.get_random_anime_gif('anime slap', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} slapped {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def stab(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime stab')
+        gif_url = await self.get_random_anime_gif('anime stab', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} stabbed {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def punch(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime punch')
+        gif_url = await self.get_random_anime_gif('anime punch', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} punched {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def kickk(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime kick')
+    async def kick(self, ctx, user: discord.Member):
+        gif_url = await self.get_random_anime_gif('anime kick', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} kicked {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def stomp(self, ctx, user: discord.Member):
-        gif_url = await self.get_random_anime_gif('anime stomp')
+        gif_url = await self.get_random_anime_gif('anime stomp', gif_only=True)
         embed = discord.Embed(description=f"{ctx.author.mention} stomped on {user.mention}")
         embed.set_image(url=gif_url)
         await ctx.send(embed=embed)
 
-    async def get_random_anime_gif(self, query):
+    async def get_random_anime_gif(self, query, gif_only=False):
         try:
             search_results = self.google_search(query)
             gif_url = random.choice(search_results)
-            return gif_url
+            if gif_only:
+                gif_url = self.ensure_gif_format(gif_url)
+            high_res_gif_url = self.get_high_resolution_url(gif_url)
+            return high_res_gif_url
         except Exception as e:
             print(f"Error fetching anime GIF: {e}")
 
@@ -75,6 +78,17 @@ class InteractionsCog(commands.Cog):
         image_urls = [element["src"] for element in image_elements if element["src"].startswith("http")]
         return image_urls
 
+    def ensure_gif_format(self, url):
+        if not url.lower().endswith('.gif'):
+            url += ".gif"
+        return url
+
+    def get_high_resolution_url(self, url):
+        if url.lower().endswith('.gif'):
+            url = url.replace("giphy.gif", "giphy.webp")
+        return url
+
 def setup(bot):
     bot.add_cog(InteractionsCog(bot))
+
 
