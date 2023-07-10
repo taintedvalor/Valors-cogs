3671ee59ff72619672581e120361dce46b135b8a
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-10 15:04:51 -0400

    Update __init__.py

--- Interaction/__init__.py
+++ Interaction/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 from redbot.core.bot import Red
 from .randimages import RandImages
+from .neko.best import RolePlayCog
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
