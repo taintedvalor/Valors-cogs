c13950ddc87a4b098c07ca3ccda02124504efd59
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-10 14:05:49 -0500

    Add files via upload

---
+++ Interaction/__init__.py
@@ -0,0 +1,13 @@
+import asyncio
+from redbot.core.bot import Red
+from .randimages import RandImages
+from .nekobest import RolePlayCog
+
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+async def setup(bot: Red):
+    cog = RandImages(bot)
+    await bot.add_cog(cog)
