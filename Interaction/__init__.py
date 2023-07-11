e43b90e75aa2368f55365552e57c3400aedaccfe
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-10 22:22:15 -0400

    Update __init__.py

--- Interaction/__init__.py
+++ Interaction/__init__.py
@@ -1,17 +1,11 @@
 import asyncio
 from redbot.core.bot import Red
-from .randimages import RandImages
+
 from .roleplaycog import RolePlayCog
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
 )
-
-
-async def setup(bot: Red):
-    cog = RandImages(bot)
-    await bot.add_cog(cog)
-
 
 async def setup(bot: Red) -> None:
     cog = RolePlayCog(bot)
