1fbb25a5c7262787a6a93174744cd994f6ca6888
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-10 22:36:37 -0500

    Add files via upload

---
+++ Interaction/constants.py
@@ -0,0 +1,61 @@
+REDDIT_BASEURL = "https://api.reddit.com/r/{}/random"
+IMGUR_LINKS = "http://imgur.com", "https://m.imgur.com", "https://imgur.com"
+GOOD_EXTENSIONS = ".png", ".jpg", ".jpeg", ".gif"
+
+ART = ["Art", "learnart", "DrugArt", "ImaginaryLovers", "conceptart", "DigitalArt", "PixelArt"]
+CUTE = [
+    "cute",
+    "Awww",
+    "babyduckgifs",
+    "rarepuppers",
+    "hardcoreaww",
+    "FromKittenToCat",
+    "cutecats",
+]
+DUCKS = ["duck", "babyduckgifs"]
+FERRETS = ["ferret", "ferrets", "FerretsGoneWild"]
+MEMES = ["dankmemes", "memes"]
+PHOTOS = [
+    "Nikon",
+    "postprocessing",
+    "streetphotography",
+    "ExposurePorn",
+    "M43",
+    "analog",
+    "photographs",
+    "mobilephotography",
+    "itookapicture",
+    "wildlifephotography",
+    "SonyAlpha",
+    "astrophotography",
+    "retouching",
+    "iPhoneography",
+    "birdpics",
+    "Astronomy",
+    "LandscapePhotography",
+    "lomography",
+    "photojournalism",
+    "pics",
+    "LondonPics",
+    "urbanexploration",
+    "ChicagoPics",
+    "Pentax",
+    "hdr",
+    "japanpics",
+    "foodphotography",
+    "blackandwhite",
+    "fashionphotography",
+    "shootingcars",
+    "nightphotography",
+    "AbstractPhotos",
+    "macro",
+]
+WALLPAPERS = [
+    "wallpaper",
+    "iphonewallpapers",
+    "MinimalWallpaper",
+    "WQHD_Wallpaper",
+    "iWallpaper",
+    "topwalls",
+    "WidescreenWallpaper",
+]
