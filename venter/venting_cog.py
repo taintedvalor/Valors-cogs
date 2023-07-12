23ba0e624caf9cc5d75220b3d736c24701173f6c
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-11 22:29:39 -0400

    Update venting_cog.py

--- venter/venting_cog.py
+++ venter/venting_cog.py
@@ -1,8 +1,6 @@
 import discord
-from redbot.core import commands, checks, bot
-from random import choice as randchoice
-from pprint import pprint as pp
-from typing import List, Dict
+from redbot.core import commands, bot
+from typing import List
 from fuzzywuzzy import process
 
 BaseCog = getattr(commands, "Cog", object)
@@ -11,6 +9,7 @@ class VentCog(commands.Cog):
     def __init__(self, bot_instance: bot):
         self.bot = bot_instance
         self.venting_channels = {}
+        self.pending_messages = {}
 
     @commands.group(invoke_without_command=True)
     async def venter(self, ctx):
@@ -62,7 +61,7 @@ class VentCog(commands.Cog):
             response = await self.bot.wait_for("message", check=check, timeout=60)
         except asyncio.TimeoutError:
             return await ctx.send("You took too long to respond. Message sending canceled.")
-        
+
         guild_name_or_id = response.content.strip()
         guild = self.find_guild(ctx, guild_name_or_id)
         if guild:
@@ -74,16 +73,45 @@ class VentCog(commands.Cog):
             if not target_channel:
                 return await ctx.send("The configured venting channel was not found in the specified guild.")
 
-            attachments = ctx.message.attachments
-            if not message and not attachments:
-                return await ctx.send("Please provide a message or an attachment.")
-
-            vent_message = f"Anonymous Person:\n{message}" if message else "Anonymous Person"
-            files = [await attachment.to_file(spoiler=self.is_image_spoiler(attachment.filename)) for attachment in attachments]
-            await target_channel.send(content=vent_message, files=files)
-            await ctx.send("Your message has been sent.")
+            await ctx.send("You can now start sending your anonymous messages. Respond with ✓ when you are done.")
+            self.pending_messages[ctx.author.id] = {
+                "target_channel": target_channel,
+                "messages": [],
+                "attachments": []
+            }
         else:
             await ctx.send("Guild not found.")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if (
+            not message.guild
+            and message.author != self.bot.user
+            and message.author.id in self.pending_messages
+        ):
+            pending = self.pending_messages[message.author.id]
+            if "inquiry_message" not in pending:
+                if message.content == "✓":
+                    await self.send_pending_messages(message.author.id)
+                else:
+                    pending["messages"].append(message.content)
+                    pending["attachments"].extend(message.attachments)
+
+    async def send_pending_messages(self, user_id):
+        pending = self.pending_messages.pop(user_id, None)
+        if not pending:
+            return
+
+        target_channel = pending["target_channel"]
+        messages = pending["messages"]
+        attachments = pending["attachments"]
+
+        if not messages and not attachments:
+            return
+
+        vent_message = "Anonymous Person:\n" + "\n".join(messages)
+        files = [await attachment.to_file(spoiler=self.is_image_spoiler(attachment.filename)) for attachment in attachments]
+        await target_channel.send(content=vent_message, files=files)
 
     def find_guild(self, ctx, name_or_id):
         guild = discord.utils.find(lambda g: g.name == name_or_id or str(g.id) == name_or_id, ctx.bot.guilds)
