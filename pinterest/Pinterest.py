84a2b106178c69ae8796f773ff0ebafc4e41310e
valor <61257192+taintedvalor@users.noreply.github.com>
2023-07-14 18:22:20 -0400

    Update Pinterest.py

--- pinterest/Pinterest.py
+++ pinterest/Pinterest.py
@@ -9,8 +9,6 @@ class PinterestScraper(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.channel_id = None  # Store the channel ID for the configured channel
-        self.search_term = None  # Store the search term for Pinterest images
-        self.scrape_pinterest.start()
 
     @commands.group()
     async def pinterest(self, ctx):
@@ -27,37 +25,31 @@ class PinterestScraper(commands.Cog):
     @pinterest.command()
     async def start(self, ctx, *, search_term: str):
         """Start the Pinterest image scraping."""
-        self.search_term = search_term
-        await ctx.send(f"Pinterest scraping started with search term: {search_term}.")
-    
-    @pinterest.command()
-    async def stop(self, ctx):
-        """Stop the Pinterest image scraping."""
-        self.search_term = None
-        await ctx.send("Pinterest scraping stopped.")
+        if self.channel_id is None:
+            await ctx.send("Channel not set. Use the `pinterest set_channel` command first.")
+            return
 
-    @tasks.loop(seconds=15)
-    async def scrape_pinterest(self):
-        if self.channel_id is not None and self.search_term is not None:
-            channel = self.bot.get_channel(self.channel_id)
-            if channel is not None:
-                # Scrape Pinterest for the specified search term
-                pinterest_url = f"https://www.pinterest.com/search/pins/?q={self.search_term}"
-                response = requests.get(pinterest_url)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                image_elements = soup.find_all('img')
-                random_image = random.choice(image_elements)
+        pinterest_url = f"https://www.pinterest.com/search/pins/?q={search_term}"
+        response = requests.get(pinterest_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        image_elements = soup.find_all('img')
+        
+        if not image_elements:
+            await ctx.send("No images found for the specified search term.")
+            return
+        
+        random_image = random.choice(image_elements)
+        image_url = random_image['src']
 
-                # Create an embed with the image link
-                embed = discord.Embed()
-                embed.set_image(url=random_image['src'])
+        channel = self.bot.get_channel(self.channel_id)
+        if channel is not None:
+            embed = discord.Embed()
+            embed.set_image(url=image_url)
 
-                # Post the embed in the configured channel
-                await channel.send(embed=embed)
-
-    @scrape_pinterest.before_loop
-    async def before_scrape_pinterest(self):
-        await self.bot.wait_until_ready()
+            await channel.send(embed=embed)
+            await ctx.send("Image sent to the configured channel.")
+        else:
+            await ctx.send("Invalid channel. Make sure the bot has access to the specified channel.")
 
 def setup(bot):
     bot.add_cog(PinterestScraper(bot))
