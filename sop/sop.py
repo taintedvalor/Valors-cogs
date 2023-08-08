ba0de972622e73d59f01951ed3324ffb7a59016f
valor <61257192+taintedvalor@users.noreply.github.com>
2023-08-08 14:17:57 -0400

    Update sop.py

--- sop/sop.py
+++ sop/sop.py
@@ -1,11 +1,17 @@
 import discord
 from redbot.core import commands
 import random
+import praw
 
 class SmashOrPass(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.games = {}
+        self.reddit = praw.Reddit(
+            client_id='BRjqCTnhB_76_ucTXV4J9A',
+            client_secret='8lU5K417dv97YjqZ1VBqgxwQVEIoFw',
+            user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        )
 
     @commands.group()
     async def smashorpass(self, ctx):
@@ -13,18 +19,22 @@ class SmashOrPass(commands.Cog):
             await ctx.send("Invalid subcommand. Use `!help smashorpass` for assistance.")
 
     @smashorpass.command()
-    async def start(self, ctx, term, num_images=10, rounds=10, nsfw=False):
+    async def start(self, ctx, term, num_rounds=10, nsfw=False):
         if not ctx.channel.is_nsfw() and nsfw:
             await ctx.send("This command can only be used in NSFW channels.")
             return
 
-        images = self.get_images_from_search(term, num_images)  # Implement this function
-        self.games[ctx.channel.id] = {'images': images, 'index': 0, 'votes': {}, 'rounds': rounds}
+        images = self.get_images_from_reddit(term, num_rounds)  # Implement this function
+        if not images:
+            await ctx.send("No images found.")
+            return
+
+        self.games[ctx.channel.id] = {'images': images, 'index': 0, 'votes': {}, 'rounds': num_rounds}
         await self.show_image(ctx)
 
     async def show_image(self, ctx):
         game_data = self.games[ctx.channel.id]
-        if game_data['index'] >= len(game_data['images']):
+        if game_data['index'] >= game_data['rounds']:
             await self.end_game(ctx)
             return
 
@@ -48,11 +58,19 @@ class SmashOrPass(commands.Cog):
         # Display voting results
         result_text = "Voting Results:\n"
         for idx, vote in game_data['votes'].items():
-            result_text += f"Image {idx + 1}: {vote}\n"
+            result_text += f"Round {idx + 1}: {vote}\n"
         await ctx.send(result_text)
 
         # Clean up
         del self.games[ctx.channel.id]
+
+    def get_images_from_reddit(self, term, num_images):
+        subreddit = self.reddit.subreddit('all')  # You can change to a specific subreddit
+        images = []
+        for submission in subreddit.search(query=term, sort='top', limit=num_images):
+            if submission.url.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+                images.append(submission.url)
+        return images
 
 def setup(bot):
     bot.add_cog(SmashOrPass(bot))
