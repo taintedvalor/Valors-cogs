8a057c6a3f0f94ba606d2a7e8156ee481645f1ac
valor <61257192+taintedvalor@users.noreply.github.com>
2023-08-08 11:42:09 -0400

    Update __init__.py

--- SmashOrPass/__init__.py
+++ SmashOrPass/__init__.py
@@ -1,11 +1,5 @@
-from redbot.core.bot import Red
-from .randimages import SmashOrPass
 
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
-
-async def setup(bot: Red):
-    cog = SmashOrPass(bot)
-    await bot.add_cog(cog)
+from .randimages import WompCog
+  
+ async def setup(bot): 
+     await bot.add_cog(WompCog(bot))
