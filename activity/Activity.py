e0f8701b6fbfa455afe7c7959274c7ddda919449
valor <61257192+taintedvalor@users.noreply.github.com>
2023-08-09 09:12:24 -0400

    Update Activity.py

--- activity/Activity.py
+++ activity/Activity.py
@@ -1,100 +1,108 @@
 import discord
-from redbot.core import commands, Config, checks, embeds
+from redbot.core import commands, Config, checks
+import random
+import asyncio
 
-class Activity(commands.Cog):
+class Engagement(commands.Cog):
+    """Server engagement with random questions."""
+    
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)  # Change this identifier
+        self.config = Config.get_conf(self, identifier=1234567890)
+        
         default_settings = {
             "questions": [],
-            "interval": 24,  # Default interval in hours
             "role": None,
+            "interval": 0,
             "channel": None,
             "active": False
         }
+        
         self.config.register_guild(**default_settings)
-
+        self.question_task = self.bot.loop.create_task(self.random_question())
+        
     @commands.group()
+    @checks.admin()
     async def activity(self, ctx):
-        """Manage the Activity cog settings."""
+        """Manage server engagement settings."""
         pass
     
-    @activity.command()
-    async def add(self, ctx, *, question: str):
-        """Add a question to the list."""
+    @activity.command(name="add")
+    async def activity_add(self, ctx, *, question: str):
+        """Add a question for server engagement."""
         questions = await self.config.guild(ctx.guild).questions()
-        questions.append(question)
-        await self.config.guild(ctx.guild).questions.set(questions)
-        await ctx.send(f"Question added: {question}")
-    
-    @activity.command()
-    async def remove(self, ctx, *, question: str):
-        """Remove a question from the list."""
-        questions = await self.config.guild(ctx.guild).questions()
-        if question in questions:
+        if question not in questions:
+            questions.append(question)
+            await self.config.guild(ctx.guild).questions.set(questions)
+            await ctx.send(f"Added question: {question}")
+        else:
             questions.remove(question)
             await self.config.guild(ctx.guild).questions.set(questions)
-            await ctx.send(f"Question removed: {question}")
-        else:
-            await ctx.send("Question not found.")
-    
-    @activity.command()
-    async def settings(self, ctx):
-        """View cog settings."""
-        settings = await self.config.guild(ctx.guild).all()
-        
-        embed = embeds.Embed(title="Activity Cog Settings", color=discord.Color.blue())
-        embed.add_field(name="Interval", value=f"{settings['interval']} hours")
-        
-        role_id = settings["role"]
-        role = ctx.guild.get_role(role_id)
-        embed.add_field(name="Ping Role", value=role.mention if role else "Not set")
-        
-        channel_id = settings["channel"]
-        channel = ctx.guild.get_channel(channel_id)
-        embed.add_field(name="Question Channel", value=channel.mention if channel else "Not set")
-        
-        questions = settings["questions"]
-        if questions:
-            questions_str = "\n".join(questions)
-            embed.add_field(name="Questions", value=questions_str)
-        else:
-            embed.add_field(name="Questions", value="No questions")
-        
-        active = settings["active"]
-        embed.add_field(name="Active", value="Yes" if active else "No")
-        
-        await ctx.send(embed=embed)
-    
-    @activity.command()
-    async def interval(self, ctx, hours: int):
-        """Set the question interval."""
-        await self.config.guild(ctx.guild).interval.set(hours)
-        await ctx.send(f"Question interval set to {hours} hours.")
-    
-    @activity.command()
-    async def role(self, ctx, role: discord.Role):
-        """Set the role to ping."""
+            await ctx.send(f"Removed question: {question}")
+            
+    @activity.command(name="role")
+    async def activity_role(self, ctx, role: discord.Role):
+        """Set the role to be pinged for questions."""
         await self.config.guild(ctx.guild).role.set(role.id)
-        await ctx.send(f"Ping role set to {role.name}.")
-    
-    @activity.command()
-    async def channel(self, ctx, channel: discord.TextChannel):
-        """Set the question channel."""
+        await ctx.send(f"Activity role set to: {role.mention}")
+        
+    @activity.command(name="interval")
+    async def activity_interval(self, ctx, hours: int):
+        """Set the interval for sending questions (in hours)."""
+        await self.config.guild(ctx.guild).interval.set(hours)
+        await ctx.send(f"Question interval set to: {hours} hours")
+        self.question_task.cancel()
+        self.question_task = self.bot.loop.create_task(self.random_question())
+        
+    @activity.command(name="channel")
+    async def activity_channel(self, ctx, channel: discord.TextChannel):
+        """Set the channel for sending questions."""
         await self.config.guild(ctx.guild).channel.set(channel.id)
-        await ctx.send(f"Question channel set to {channel.mention}.")
-    
-    @activity.command()
-    async def start(self, ctx):
-        """Start sending questions."""
-        await self.config.guild(ctx.guild).active.set(True)
-        # Start a loop to send questions at the specified interval
-    
-    @activity.command()
-    async def stop(self, ctx):
-        """Stop sending questions."""
-        await self.config.guild(ctx.guild).active.set(False)
-        # Stop the loop that sends questions
+        await ctx.send(f"Question channel set to: {channel.mention}")
+        
+    @activity.command(name="toggle")
+    async def activity_toggle(self, ctx):
+        """Toggle sending questions on/off."""
+        active = await self.config.guild(ctx.guild).active()
+        await self.config.guild(ctx.guild).active.set(not active)
+        if not active:
+            await ctx.send("Questions are now on")
+        else:
+            await ctx.send("Questions are now off")
+            
+    @activity.command(name="settings")
+    async def activity_settings(self, ctx, setting: str = None):
+        """View or configure activity settings."""
+        if setting:
+            if setting == "interval":
+                interval = await self.config.guild(ctx.guild).interval()
+                await ctx.send(f"Current interval: {interval} hours")
+            elif setting == "role":
+                role_id = await self.config.guild(ctx.guild).role()
+                role = ctx.guild.get_role(role_id)
+                await ctx.send(f"Current role: {role.mention}")
+        else:
+            interval = await self.config.guild(ctx.guild).interval()
+            role_id = await self.config.guild(ctx.guild).role()
+            role = ctx.guild.get_role(role_id)
+            await ctx.send(f"Current settings:\nInterval: {interval} hours\nRole: {role.mention}")
+            
+    async def random_question(self):
+        await self.bot.wait_until_ready()
+        while True:
+            for guild in self.bot.guilds:
+                active = await self.config.guild(guild).active()
+                if active:
+                    role_id = await self.config.guild(guild).role()
+                    role = guild.get_role(role_id)
+                    channel_id = await self.config.guild(guild).channel()
+                    channel = guild.get_channel(channel_id)
+                    questions = await self.config.guild(guild).questions()
+                    if role and channel and questions:
+                        question = random.choice(questions)
+                        await channel.send(f"{role.mention} {question}")
+            interval = await self.config.guild(guild).interval()
+            await asyncio.sleep(interval * 3600)
 
 def setup(bot):
-    bot.add_cog(Activity(bot))
+    bot.add_cog(Engagement(bot))
