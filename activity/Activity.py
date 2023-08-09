bff4e456f95f848ba7da4e7c42c7de7901683ff1
valor <61257192+taintedvalor@users.noreply.github.com>
2023-08-09 09:22:16 -0400

    Update Activity.py

--- activity/Activity.py
+++ activity/Activity.py
@@ -15,8 +15,7 @@ class Engagement(commands.Cog):
             "role": None,
             "interval": 0,
             "channel": None,
-            "active": False,
-            "last_question_time": 0
+            "active": False
         }
         
         self.config.register_guild(**default_settings)
@@ -37,30 +36,73 @@ class Engagement(commands.Cog):
             await self.config.guild(ctx.guild).questions.set(questions)
             await ctx.send(f"Added question: {question}")
         else:
-            await ctx.send(f"This question is already in the database.")
+            questions.remove(question)
+            await self.config.guild(ctx.guild).questions.set(questions)
+            await ctx.send(f"Removed question: {question}")
             
-    # ... (other command methods remain the same)
-
+    @activity.command(name="role")
+    async def activity_role(self, ctx, role: discord.Role):
+        """Set the role to be pinged for questions."""
+        await self.config.guild(ctx.guild).role.set(role.id)
+        await ctx.send(f"Activity role set to: {role.mention}")
+        
+    @activity.command(name="interval")
+    async def activity_interval(self, ctx, hours: int):
+        """Set the interval for sending questions (in hours)."""
+        await self.config.guild(ctx.guild).interval.set(hours)
+        await ctx.send(f"Question interval set to: {hours} hours")
+        self.question_task.cancel()
+        self.question_task = self.bot.loop.create_task(self.random_question())
+        
+    @activity.command(name="channel")
+    async def activity_channel(self, ctx, channel: discord.TextChannel):
+        """Set the channel for sending questions."""
+        await self.config.guild(ctx.guild).channel.set(channel.id)
+        await ctx.send(f"Question channel set to: {channel.mention}")
+        
+    @activity.command(name="toggle")
+    async def activity_toggle(self, ctx):
+        """Toggle sending questions on/off."""
+        active = await self.config.guild(ctx.guild).active()
+        await self.config.guild(ctx.guild).active.set(not active)
+        if not active:
+            await ctx.send("Questions are now on")
+        else:
+            await ctx.send("Questions are now off")
+            
+    @activity.command(name="settings")
+    async def activity_settings(self, ctx, setting: str = None):
+        """View or configure activity settings."""
+        if setting:
+            if setting == "interval":
+                interval = await self.config.guild(ctx.guild).interval()
+                await ctx.send(f"Current interval: {interval} hours")
+            elif setting == "role":
+                role_id = await self.config.guild(ctx.guild).role()
+                role = ctx.guild.get_role(role_id)
+                await ctx.send(f"Current role: {role.mention}")
+        else:
+            interval = await self.config.guild(ctx.guild).interval()
+            role_id = await self.config.guild(ctx.guild).role()
+            role = ctx.guild.get_role(role_id)
+            await ctx.send(f"Current settings:\nInterval: {interval} hours\nRole: {role.mention}")
+            
     async def random_question(self):
         await self.bot.wait_until_ready()
         while True:
             for guild in self.bot.guilds:
                 active = await self.config.guild(guild).active()
                 if active:
-                    current_time = time.time()
-                    last_question_time = await self.config.guild(guild).last_question_time()
-                    interval = await self.config.guild(guild).interval()
-                    if current_time - last_question_time >= interval * 3600:
-                        role_id = await self.config.guild(guild).role()
-                        role = guild.get_role(role_id)
-                        channel_id = await self.config.guild(guild).channel()
-                        channel = guild.get_channel(channel_id)
-                        questions = await self.config.guild(guild).questions()
-                        if role and channel and questions:
-                            question = random.choice(questions)
-                            await channel.send(f"{role.mention} {question}")
-                            await self.config.guild(guild).last_question_time.set(current_time)
-            await asyncio.sleep(60)  # Check every minute
+                    role_id = await self.config.guild(guild).role()
+                    role = guild.get_role(role_id)
+                    channel_id = await self.config.guild(guild).channel()
+                    channel = guild.get_channel(channel_id)
+                    questions = await self.config.guild(guild).questions()
+                    if role and channel and questions:
+                        question = random.choice(questions)
+                        await channel.send(f"{role.mention} {question}")
+            interval = await self.config.guild(guild).interval()
+            await asyncio.sleep(interval * 3600)
 
 def setup(bot):
     bot.add_cog(Engagement(bot))
