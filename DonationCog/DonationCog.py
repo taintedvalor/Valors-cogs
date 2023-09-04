17078aeec1fff280f52c5aff526b18b36e15c979
valor <61257192+taintedvalor@users.noreply.github.com>
2023-09-03 22:22:25 -0400

    Update DonationCog.py

--- DonationCog/DonationCog.py
+++ DonationCog/DonationCog.py
@@ -18,21 +18,5 @@ class DonationCog(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @commands.slash.command(
-        name="donate",
-        description="Display a donation link.",
-    )
-    async def slashdonate(self, ctx: commands.Context):
-        # Replace 'YOUR_DONATION_LINK' with your actual donation link
-        donation_link = 'https://cash.app/$taintedvalor'
-
-        embed = discord.Embed(
-            title="Donate",
-            description=f"Support the server by donating!\n[Click here to donate]({donation_link})",
-            color=discord.Color.green()
-        )
-
-        await ctx.send(embed=embed)
-
 def setup(bot):
     bot.add_cog(DonationCog(bot))
