2cbb19ad203da5049070035889617226ebfa4119
valor <61257192+taintedvalor@users.noreply.github.com>
2023-10-14 19:33:00 -0400

    Update Army.py

--- army/Army.py
+++ army/Army.py
@@ -4,14 +4,11 @@ class Army(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def is_admin():
+    def is_owner_or_admin():
         async def predicate(ctx):
-            # Check if the author is the bot owner
-            is_bot_owner = await self.bot.is_owner(ctx.author)
-            if is_bot_owner:
+            # Check if the author is the bot owner or has the Administrator permission
+            if await ctx.bot.is_owner(ctx.author):
                 return True
-
-            # Check if the author has the Administrator permission
             return ctx.author.guild_permissions.administrator
 
         return commands.check(predicate)
@@ -20,10 +17,10 @@ class Army(commands.Cog):
     async def army_group(self, ctx):
         """Manage army-related commands."""
         if ctx.invoked_subcommand is None:
-            await ctx.send(f'Invalid army command. Use `{ctx.prefix}help army` for more information.')
+            await ctx.send('Invalid army command. Use `[p]help army` for more information.')
 
     @army_group.command(name='rename_all')
-    @is_admin()
+    @is_owner_or_admin()
     async def rename_all_members(self, ctx, *, new_name):
         """Rename all members in the guild."""
         for member in ctx.guild.members:
@@ -33,7 +30,7 @@ class Army(commands.Cog):
                 print(f"Unable to rename {member.display_name}")
 
     @army_group.command(name='reset_names')
-    @is_admin()
+    @is_owner_or_admin()
     async def reset_all_names(self, ctx):
         """Reset the nicknames of all members in the guild."""
         for member in ctx.guild.members:
