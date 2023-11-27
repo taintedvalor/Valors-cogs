6cf0448c488ae1dd72709e15897b97ebfac11494
valor <61257192+taintedvalor@users.noreply.github.com>
2023-11-26 22:00:22 -0500

    Update womp.py

--- Womp/womp.py
+++ Womp/womp.py
@@ -27,41 +27,32 @@ class Womp(commands.Cog):
             return
 
         guild_settings = await self.config.guild(message.guild).all()
-        if guild_settings["toggle"] and ("womp" in message.content.lower() or "womp" in message.content.lower()):
+        if guild_settings["toggle"] and "womp" in message.content.lower():
             num_womps = message.content.lower().count("womp")
             min_amount = num_womps
             max_amount = num_womps + 2  # Adjust as needed
             random_amount = random.randint(min_amount, max_amount)
-
+            
             womp_effect = ''.join([
                 f"**{char}**" if char == "womp" else char
                 for char in " womp" * random_amount
             ])
-
-            # Extract emojis from the original message
-            emojis = [str(emoji) for emoji in message.content.split() if emoji.isnumeric() or emoji in discord.emojis]
-
-            # Add random emojis or include original emojis in the response
-            if random.choice([True, False]):  # Adjust the probability as needed
-                random_emojis = [random.choice(self.bot.emojis) for _ in range(random.randint(1, 3))]
-                response = f"{womp_effect} {' '.join(random_emojis)}"
-            elif emojis:
-                response = f"{womp_effect} {' '.join(emojis)}"
-            else:
-                response = womp_effect
-
+            
             if "~" in message.content:
                 punctuation = "~"
             else:
                 punctuation = random.choice(['.', '!', '?'])
-
+                
+            emojis = [':smile:', ':joy:', ':heart_eyes:', ':sunglasses:']  # Add more emojis as needed
+            random_emoji = random.choice(emojis)
+            
             capitalized_womp_effect = ' '.join([
                 word.capitalize() if i % 2 == 0 else word
-                for i, word in enumerate(response.split())
+                for i, word in enumerate(womp_effect.split())
             ])
-
-            response = f"{capitalized_womp_effect}{punctuation}"
-
+            
+            response = f"{capitalized_womp_effect} {random_emoji} {punctuation}"
+            
             await message.channel.send(response)
 
 def setup(bot):
