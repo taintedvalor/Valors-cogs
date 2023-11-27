d6799b9f6d53fb438a29abbe574f2c5c33f408d6
valor <61257192+taintedvalor@users.noreply.github.com>
2023-11-26 21:57:21 -0500

    Update womp.py

--- Womp/womp.py
+++ Womp/womp.py
@@ -27,12 +27,12 @@ class Womp(commands.Cog):
             return
 
         guild_settings = await self.config.guild(message.guild).all()
-        if guild_settings["toggle"] and "womp" in message.content.lower():
+        if guild_settings["toggle"] and ("womp" in message.content.lower() or "womp" in message.content.lower()):
             num_womps = message.content.lower().count("womp")
             min_amount = num_womps
             max_amount = num_womps + 2  # Adjust as needed
             random_amount = random.randint(min_amount, max_amount)
-            
+
             womp_effect = ''.join([
                 f"**{char}**" if char == "womp" else char
                 for char in " womp" * random_amount
@@ -54,14 +54,14 @@ class Womp(commands.Cog):
                 punctuation = "~"
             else:
                 punctuation = random.choice(['.', '!', '?'])
-                
+
             capitalized_womp_effect = ' '.join([
                 word.capitalize() if i % 2 == 0 else word
                 for i, word in enumerate(response.split())
             ])
-            
+
             response = f"{capitalized_womp_effect}{punctuation}"
-            
+
             await message.channel.send(response)
 
 def setup(bot):
